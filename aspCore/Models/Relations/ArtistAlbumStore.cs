34e847a44ab5c74f5b0d3226d2ab52996f7576e5
ume05rw <ume05rw@gmail.com>
2019-06-28 00:49:37 +0900

    TSのリレーション生成ロジックを移植したが、まだ実行してない。

--- aspCore/Models/Relations/ArtistAlbumStore.cs
+++ aspCore/Models/Relations/ArtistAlbumStore.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using MusicFront.Models.Artists;
 using MusicFront.Models.Bases;
+using MusicFront.Models.JsonRpcs;
+using MusicFront.Models.Mopidies;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicFront.Models.Relations
@@ -15,7 +21,39 @@ namespace MusicFront.Models.Relations
             this.Dbc.ArtistAlbums.RemoveRange(this.Dbc.ArtistAlbums);
             await this.Dbc.SaveChangesAsync();
 
+            var tasks = new List<Task<bool>>();
+            var artists = this.Dbc.Artists.ToArray();
 
+            foreach (var artist in artists)
+                tasks.Add(this.AddAlbumsByArtist(artist));
+
+            await Task.WhenAll(tasks);
+
+            return true;
+        }
+
+        private async Task<bool> AddAlbumsByArtist(Artist artist)
+        {
+            var args = new MethodArgs(artist.Uri);
+            var request = JsonRpcFactory.CreateRequest(Methods.LibraryBrowse, args);
+
+            var resultObject = await this.QueryMopidy(request);
+            var result = JArray.FromObject(resultObject).ToObject<List<Ref>>();
+
+            foreach (var row in result)
+            {
+                var album = this.Dbc.Albums.FirstOrDefault(e => e.Uri == row.uri);
+
+                if (album == null)
+                    continue;
+
+                this.Dbc.ArtistAlbums.Add(new ArtistAlbum() {
+                    ArtistId = artist.Id,
+                    AlbumId = album.Id
+                });
+            }
+
+            await this.Dbc.SaveChangesAsync();
 
             return true;
         }
--- aspCore/Models/Relations/GenreAlbumStore.cs
+++ aspCore/Models/Relations/GenreAlbumStore.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using MusicFront.Models.Bases;
+using MusicFront.Models.Genres;
+using MusicFront.Models.JsonRpcs;
+using MusicFront.Models.Mopidies;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicFront.Models.Relations
@@ -15,7 +21,48 @@ namespace MusicFront.Models.Relations
             this.Dbc.GenreAlbums.RemoveRange(this.Dbc.GenreAlbums);
             await this.Dbc.SaveChangesAsync();
 
+            var tasks = new List<Task<bool>>();
+            var genres = this.Dbc.Genres.ToList();
 
+            foreach (var genre in this.Dbc.Genres.ToArray())
+                tasks.Add(this.AddAlbumsByGenre(genre));
+
+            await Task.WhenAll(tasks);
+
+            return true;
+        }
+
+        private async Task<bool> AddAlbumsByGenre(Genre genre)
+        {
+            var args = new MethodArgs(genre.Uri);
+            var request = JsonRpcFactory.CreateRequest(Methods.LibraryBrowse, args);
+
+            var resultObject = await this.QueryMopidy(request);
+            var result = JArray.FromObject(resultObject).ToObject<List<Ref>>();
+
+            foreach (var row in result)
+            {
+                var refQuery = row.uri.Split('?')[1];
+                var uriParams = refQuery.Split('&')
+                    .Where(e => e.StartsWith("album=")).FirstOrDefault();
+
+                if (uriParams == null)
+                    continue;
+
+                var albumUri = uriParams.Split('=')[1];
+
+                var album = this.Dbc.Albums.FirstOrDefault(e => e.Uri == albumUri);
+
+                if (album == null)
+                    continue;
+
+                this.Dbc.GenreAlbums.Add(new GenreAlbum() {
+                    GenreId = genre.Id,
+                    AlbumId = album.Id
+                });
+            }
+
+            await this.Dbc.SaveChangesAsync();
 
             return true;
         }
