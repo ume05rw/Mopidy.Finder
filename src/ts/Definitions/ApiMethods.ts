ecf5f48957279c90b498133c967632dcb974b9ae
ume05rw <ume05rw@gmail.com>
2019-06-29 20:00:30 +0900

    TS側モデルのフォルダ構造をメンテナンス。

--- src/ts/Definitions/ApiMethods.ts
+++
@@ -1,4 +0,0 @@
-export default class ApiMethods {
-    public static readonly LibrarySearch: string = 'core.library.search';
-    public static readonly LibraryBrowse: string = 'core.library.browse';
-}
---
+++ src/ts/Models/Mopidies/Methods/Library.ts
@@ -0,0 +1,4 @@
+export default class Library {
+    public static readonly Search: string = 'core.library.search';
+    public static readonly Browse: string = 'core.library.browse';
+}
